13e3863aa8d7d1a97e2b4ed597a9e15c3ba6106d
zoligal <zoligal@yandex.ru>
2026-08-24 18:02:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-sleep-tracker
+Repository for homework project.
---
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.sleeptracker;
+
+public class SleepTrackerApp {
+
+    public static void main(String[] args) {
+
+    }
+}
